218aed44c54fd97e50804d83052765c9111c4338
Raul <rlkandela@gmail.com>
2020-04-07 05:37:53 +0200

    the original synchronized items stoped being a constant due to possible change during runtime, changed get_content function that wasn't working well, added a function that returns the items configured for synchronization but with space checking, functions for checking if a folder expanded or contracted, a function that combines the expansion and the contraction in only one function, and functions that returns the items that wont be needed to upload or reforward

--- src/folder_checking.py
+++ src/folder_checking.py
@@ -3,13 +3,15 @@
 import os
 import system_config
 
-SYNC_ITEMS = system_config.get_synchronized_items()
 
 def get_content(folder):
     content = os.listdir(folder)
     ret = []
     for item in content:
-        ret.append(os.path.join(folder,item))
+        joined = os.path.join(folder,item)
+        if os.path.isdir(joined):
+            joined+='/'
+        ret.append(joined)
     return ret
 
 def get_folder_size(folder):
@@ -36,6 +38,52 @@ def get_valid_items(or_item, max_size):
                     ret.append(i)
     else:
         ret.append(or_item)
-
     return ret
 
+def get_sync_valid_items(max_size):
+    items = system_config.get_synchronized_items()
+    ret = []
+    for item in items:
+        ret+=get_valid_items(item,max_size)
+    return ret
+
+def check_folder_expanded(folder, local_items):
+    items = [item for item in local_items if item.startswith(folder)]
+    if len(items) == 0:
+        return False
+    elif len(items) == 1:
+        if items[0] == folder:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+def check_folder_contracted(folder, uploaded_items):
+    items = [item for item in uploaded_items if item.startswith(folder)]
+    if len(items) == 0:
+        return False
+    elif len(items) == 1:
+        if items[0] == folder:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+def calculate_diff(uploaded_items):
+    local_items = get_sync_valid_items(system_config.MAX_DIR_SIZE)
+    local_folders = [item for item in local_items if os.path.isdir(item)]
+    uploaded_folders = [item for item in uploaded_items if os.path.isdir(item)]
+
+    expanded = [folder for folder in uploaded_folders if check_folder_expanded(folder, local_items)]
+    contracted = [folder for folder in local_folders if check_folder_contracted(folder, uploaded_items)]
+    return (expanded,contracted)
+
+# Se subiran las devueltas, no se reenviaran las expanded
+def process_expanded(expanded_folders, local_items):
+    return [item for folder in expanded_folders for item in local_items if item.startswith(folder)]
+
+# Se subiran las contracted, no se reenviaran las devueltas
+def process_contracted(contracted_folders, uploaded_items):
+    return [item for folder in contracted_folders for item in uploaded_items if item.startswith(folder)]
