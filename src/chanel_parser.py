2528f9f1b470c5d807e5ec813d3f59984b46f663
Raul <rlkandela@gmail.com>
2020-04-08 04:32:18 +0200

    added docstring and removed an include and a useless variable

--- src/chanel_parser.py
+++ src/chanel_parser.py
@@ -1,12 +1,19 @@
 # coding: utf-8
-"""File for retrieving from the channel the uploaded files and its locations"""
+"""File for retrieving from the channel the uploaded files and its locations but not for downloading them"""
 import os
-import telethon
 import api_config
 import folder_checking
 
 async def get_messages(client):
-    ret = []
+    """Function that gets the messages sent on the channel, but just the last n specified by the last integer send. Must be awaited
+
+    Args:
+        client (TelegramClient): The already connected client
+
+    Returns:
+        list of Message: List of the objects of the messages read from the channel, if the channel does not exists, it returns an empti list and creates the channel
+    """
+
     dialogs = await client.get_dialogs(archived=None)
     names = [i.name for i in dialogs]
     if api_config.CHANNEL_NAME in names:
@@ -21,12 +28,39 @@ async def get_messages(client):
         return []
 
 def get_filenames(msgs):
+    """Function that return the filenames of the items uploaded
+
+    Args:
+        msgs (list of Message): List of messages that contain an item
+
+    Returns:
+        list of str: List of strings with the filenames of the items uploaded
+    """
+
     return [msg.media.document.attributes[0].file_name for msg in msgs]
 
 def get_paths(msgs):
+    """Function that returns the original location of the items uploaded
+
+    Args:
+        msgs (list of Message): List of messages that contain an item
+
+    Returns:
+        list of str: List of strings with the original locations of the items uploaded
+    """
+
     return [msg.message for msg in msgs]
 
 async def parse_uploaded_items(client):
+    """Function that requests the messages and returns them along the original path. Must be awaited
+
+    Args:
+        client (TelegramClient): The already connected client
+
+    Returns:
+        (list of Message,list of str): Messages containing the uploaded items and paths to where they should go
+    """
+
     msgs = await get_messages(client)
     filenames = get_filenames(msgs)
     uploaded_items = get_paths(msgs)
